02f0e3f02c776319e4410281dcc74a2aae40c69d
carlos zomer <zomcarlosdev@gmail.com>
2022-11-09 14:44:33 -0300

    controller p/ registro de cursos, exemplificação com um curso p/ utilização da API;

--- crud-spring/src/main/java/com/carlos/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/carlos/crudspring/CrudSpringApplication.java
@@ -1,7 +1,11 @@
 package com.carlos.crudspring;
 
+import com.carlos.crudspring.model.Course;
+import com.carlos.crudspring.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +14,16 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
+	}
 }
--- crud-spring/src/main/java/com/carlos/crudspring/controller/CoursesController.java
+++ crud-spring/src/main/java/com/carlos/crudspring/controller/CoursesController.java
@@ -1,6 +1,8 @@
 package com.carlos.crudspring.controller;
 
 import com.carlos.crudspring.model.Course;
+import com.carlos.crudspring.repository.CourseRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +11,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CoursesController {
+
+    private final CourseRepository courseRepository;
 
     @GetMapping
     public List<Course> list() {
-        return null;
+        return courseRepository.findAll();
     }
 }
