a1cbf7c8edb80b8f0c131c5ead04abafd6c6a160
carlos zomer <zomcarlosdev@gmail.com>
2022-11-09 15:38:07 -0300

    criação de proxy, link back/front

--- crud-spring/src/main/java/com/carlos/crudspring/controller/CoursesController.java
+++ crud-spring/src/main/java/com/carlos/crudspring/controller/CoursesController.java
@@ -5,6 +5,7 @@ import com.carlos.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +18,8 @@ public class CoursesController {
     private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
+
         return courseRepository.findAll();
     }
 }
--- crud-spring/src/main/java/com/carlos/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/carlos/crudspring/model/Course.java
@@ -1,5 +1,6 @@
 package com.carlos.crudspring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ public class Course {
 
     @Id //indica que será a primary key
     @GeneratedValue(strategy = GenerationType.AUTO) //indica que será gerado um valor aleatório
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
